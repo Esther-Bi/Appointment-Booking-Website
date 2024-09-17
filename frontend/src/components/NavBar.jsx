9588c806b962682a050e99b4bb1501e5c4df94ed
Esther Binnes <estherbinnes@gmail.com>
2024-09-17 12:36:26 +0300

    complete navigation bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 
 const NavBar = () => {
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false);
+    const [token, setToken] = useState(true);
+
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img src={assets.logo} alt="" />
-      <ul>
-        <NavLink>
-            <li>HOME</li>
-            <hr />
+      <img className='cursor-pointer' src={assets.logo} alt="" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink to='/'>
+            <li className='py-1'>HOME</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-        <NavLink>
-            <li>ALL SERVICES</li>
-            <hr />
+        <NavLink to='/services'>
+            <li className='py-1'>ALL SERVICES</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-        <NavLink>
-            <li>ABOUT</li>
-            <hr />
+        <NavLink to='/about'>
+            <li className='py-1'>ABOUT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-        <NavLink>
-            <li>COMTACT</li>
-            <hr />
+        <NavLink to='/contact'>
+            <li className='py-1'>CONTACT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
       </ul>
-      <div>
-        <button>Creat Account</button>
+      <div className='flex items-center gap-4'>
+        {
+            token
+            ? <div className='flex items-center gap-1 cursor-pointer group relative'>
+                <img className='w-10 rounded-full' src={assets.profile_pic} alt="" />
+                <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+                <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                    <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                        <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                        <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                        <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                    </div>
+                </div>
+            </div>
+            : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>CREAT ACCOUNT</button>
+        }
       </div>
     </div>
   )
