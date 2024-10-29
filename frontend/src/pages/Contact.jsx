7286591bac35c7777c860bb1ba22293091f63727
Esther Binnes <estherbinnes@gmail.com>
2024-10-29 13:48:38 +0200

    contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'>ME</span></p>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.header_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className='text-gray-500'>HaYarden 34, Tel Aviv, Israel</p>
+          <p className='text-gray-500'>Tel: +972-54-603-8616<br />Email: estherbinnes@gmail.com</p>
+        </div>
+      </div>
     </div>
   )
 }
