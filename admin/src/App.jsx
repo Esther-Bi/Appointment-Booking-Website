84896d1bc3c0f1d312002c5dbeaeb4438a623b6a
Esther Binnes <estherbinnes@gmail.com>
2024-11-24 10:35:49 +0200

    side bar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,18 +3,37 @@ import Login from "./pages/Login";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/NavBar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddService from "./pages/Admin/AddService";
+import ServicesList from "./pages/Admin/ServicesList";
 
 const App = () => {
   const {aToken} = useContext(AdminContext)
 
   return aToken ? (
-    <div>
-      <ToastContainer/>
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-service' element={<AddService />} />
+          <Route path='/services-list' element={<ServicesList />} />
+        </Routes>
+        
+      </div>
     </div>
   ) : (
     <>
     <Login />
-    <ToastContainer/>
+    <ToastContainer />
     </>
   )
 }
---
+++ admin/src/pages/Admin/ServicesList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ServicesList = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default ServicesList
